3ba328d473432c749766c4064345d0916037b9bd
Ali Akbari P <aliakbaripuloo@gmail.com>
2017-11-15 18:22:26 +0700

    Creating master list fragment

---
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.android_me.R;
+
+/**
+ * Created by albar on 15/11/2017.
+ */
+// This activity is responsible for displaying the master list of all images
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
